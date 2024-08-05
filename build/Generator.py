f5e3f3b9286d4a8178cce97bb6f78684c53f77d5
33sah <107648256+33sah@users.noreply.github.com>
2024-08-05 16:50:51 +0100

    Update Generator.py
    changed to add auto changing the macro file

--- build/Generator.py
+++ build/Generator.py
@@ -8,8 +8,8 @@ def seq(start, end, step):
     sample_count = int(abs(end - start) / step)
     return itertools.islice(itertools.count(start, step), sample_count)
 
-# Changing run.mac file for correct shape / energy
-def texteditor(file, Energy, linenumbers):
+# Changing file for correct shape / energy
+def texteditor(i, Energy, linenumbers):
     if all(type(x) in [int] for x in linenumbers):
     # Checking if integers passed in argument
 
@@ -19,25 +19,26 @@ def texteditor(file, Energy, linenumbers):
         lines = text.split("\n")
         lines[linenumbers[0]] = f"/gps/energy {Energy} MeV"
         FileEnergy = str(Energy).replace(".","_")
-        lines[linenumbers[1]] = f"/analysisDNA/fileName cancer{FileEnergy}.root"
+        lines[linenumbers[1]] = f"/analysisDNA/fileName cancer{i}_ener{FileEnergy}.root"
         final_text = "\n".join(lines)
     
         with open("file", "w") as file:
             file.write(final_text)
 
 def main():
-  # Run run.mac with gears
-  file = "cancer1.mac"
-  command = f"cancer.exe -m {file} -t 2 -p 2"
-
-  # Line Numbers to change - Geometry, Energy Range + Distribution and Data - Remember these will be 1 smaller than actuality due to Python list starting at index = 0
+  # Line Numbers to change - Energy Range and Data - Remember these will be 1 smaller than actuality due to Python list starting at index = 0
   Numbers = [53, 61]
   
-  for x in seq(5.0,9.1,0.1):
-        # Python rounding error
-        texteditor(file, round(x,2), Numbers)
-        subprocess.run(command, shell=True)
-        # Run command
+  for i in range(1, 4):
+    # Run run.mac with exe
+    command = f"cancer.exe -m cancer{i}.mac -t 2 -p 2"
+    # Note that the following energy range is for radio_endotherapy, for proton these parameters would be changed.
+    for x in seq(5.0,9.1,0.1):
+            # Python rounding error
+            texteditor(i, round(x,2), Numbers)
+            subprocess.run(command, shell=True)
+            # Run command
 
 if __name__ == "__main__":
   main()
+
